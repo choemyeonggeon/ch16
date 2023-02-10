152d8f5c56da6cbe24b63a499ca9570174794031
tjoeun <tjoeun@DESKTOP-PJLQ947>
2023-02-10 10:48:19 +0900

    연습

---
+++ src/com/greedy/section02/template/Application01.java
@@ -0,0 +1,25 @@
+package com.greedy.section02.template;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import static com.greedy.section02.template.JDBCTemplate.getConnection;
+import static com.greedy.section02.template.JDBCTemplate.close;
+public class Application01 {
+
+	public static void main(String[] args) {
+
+		Connection con = JDBCTemplate.getConnection();
+		System.out.println(con);
+	
+//		if(con != null) {
+//			try {
+//				con.close();
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}
+//		}
+		
+		/*JDBCTemplate.*/close(con);
+	}
+
+}
